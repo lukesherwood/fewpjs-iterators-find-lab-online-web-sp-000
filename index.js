3640878a1d290dd558c789989b247fd3ebecd667
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-11 21:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const testVar = {}
 
 function superbowlWin(array) {
   let obj = array.find(e => e.result === 'W')
+<<<<<<< HEAD
   if (obj) return obj.year
+=======
+  if obj return obj.year
+>>>>>>> 6e5126adc0710ae92ff44701b9a1dc7ea02096b6
 }
